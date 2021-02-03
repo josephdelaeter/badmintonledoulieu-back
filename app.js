922329c014e38e8645f6de9feb20ba7219d08c8a
jdelaeter <joseph.delaeter@gmail.com>
2021-02-04 00:31:47 +0100

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors')
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 require('dotenv/config');
 
