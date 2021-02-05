55fd2cd980aeb21c1d0592f1cef1705d4f89767e
jdelaeter <joseph.delaeter@gmail.com>
2021-02-05 21:11:39 +0100

    deploy

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ const connect = mongoose.connect(process.env.DB_CONNECTION, {
         console.log(err);
     });
 
-app.listen(port);
+    app.listen(process.env.PORT || 3000, function(){
+        console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+      });
 
 module.exports=connect
